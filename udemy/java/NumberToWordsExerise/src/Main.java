a04e058d61c795eb401c8ebcd90aad9bc0e573c0
robdesautel <17968614+robdesautel@users.noreply.github.com>
2020-04-15 16:29:38 -0400

    restructured directory

---
+++ udemy/java/NumberToWordsExerise/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Test Data
+//        for(int i = 0; i < 1000; i++){
+//            NumberToWords.numberToWords(i);
+//            System.out.println(i);
+//
+//        }
+        NumberToWords.numberToWords(-22);
+
+    }
+}
