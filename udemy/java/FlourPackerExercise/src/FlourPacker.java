20fa9dd5771b835c6e6e697214a1dccbdbc698ad
robdesautel <17968614+robdesautel@users.noreply.github.com>
2020-04-15 21:20:32 -0400

    completed exercise added instructions for the exercise

--- udemy/java/FlourPackerExercise/src/FlourPacker.java
+++ udemy/java/FlourPackerExercise/src/FlourPacker.java
@@ -1,2 +1,18 @@
 public class FlourPacker {
+    public static boolean canPack(int bigCount, int smallCount, int goal){
+        int totalKilos = 0;
+        int remainingBigCount = 0;
+        if((bigCount < 0) || (smallCount < 0)){
+            return false;
+        }
+        while(bigCount > 0){
+            totalKilos += 5;
+            bigCount --;
+        }
+        while (smallCount > 0){
+            totalKilos++;
+            smallCount--;
+        }
+        return totalKilos >= goal;
+    }
 }
--- udemy/java/FlourPackerExercise/src/Main.java
+++ udemy/java/FlourPackerExercise/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(FlourPacker.canPack(-3, 2, 12));
     }
 }
