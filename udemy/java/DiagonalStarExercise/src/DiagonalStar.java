19a27086d7ecd7316bf3622e4815a92be47a0e8e
robdesautel <17968614+robdesautel@users.noreply.github.com>
2020-05-03 15:21:32 -0400

    Got diagonal stars to go from left to right. Got Left and Right border for stars.

--- udemy/java/DiagonalStarExercise/src/DiagonalStar.java
+++ udemy/java/DiagonalStarExercise/src/DiagonalStar.java
@@ -5,7 +5,6 @@ public class DiagonalStar {
             System.out.println("Invalid Value");;
         }
         int row = 1;
-        int column = 1;
         int currentRow = 0;
         for (int i = number; i > 0; i--){ //Initialize loop
             currentRow++;
@@ -13,11 +12,25 @@ public class DiagonalStar {
                 for (int j = 0; j < number; j++){
                     System.out.print("*");
                 }
-                System.out.println();
             }else {
-                System.out.println("stars");
+                int column = 1;
+                for(int j = 0; j <= number; j++){
+
+                    if(column == 1 ||
+                       column == currentRow ||
+                       column == number){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+                    column++;
+                }
+                System.out.print(row);
+
             }
 
+            System.out.println();
+            row++;
         }
     }
 }
