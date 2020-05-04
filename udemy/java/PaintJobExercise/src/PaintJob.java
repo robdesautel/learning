161aec17ab09aeea07497337b5ca3a8f3c0cd6a6
robdesautel <17968614+robdesautel@users.noreply.github.com>
2020-05-04 14:56:58 -0400

    First method completed.

--- udemy/java/PaintJobExercise/src/PaintJob.java
+++ udemy/java/PaintJobExercise/src/PaintJob.java
@@ -1,2 +1,17 @@
 public class PaintJob {
+    public static int getBucketCount(double width,
+                                       double height,
+                                       double areaPerBucket,
+                                       int extraBuckets){
+
+        if(width < 1 || height < 1 || areaPerBucket < 1 || extraBuckets < 0){
+            return -1;
+        }
+
+        float areaOfWall = (float) width * (float) height;
+        float totalPaint = areaOfWall /(float)  areaPerBucket;
+        float numberOfBucketsNeededForJob = totalPaint - extraBuckets;
+        return Math.round(numberOfBucketsNeededForJob);
+
+    }
 }
