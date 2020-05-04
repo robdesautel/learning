97b72425277f113e84f4c7b54464c0c6c547f0ec
robdesautel <17968614+robdesautel@users.noreply.github.com>
2020-05-04 11:10:55 -0400

    Completed Project

--- udemy/java/InputCalculatorExercise/src/InputCalculator.java
+++ udemy/java/InputCalculatorExercise/src/InputCalculator.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class InputCalculator {
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        long average = 0L;
+        int counter = 0;
+        float avgrslt = 0.0F;
+
+        while (scanner.hasNextInt()){
+            int number = scanner.nextInt();
+            sum += number;
+            counter++;
+        }
+        avgrslt = (float) sum / counter;
+        average = Math.round(avgrslt);
+        System.out.println("SUM = "+ sum + " AVG = " + average);
+        scanner.close();
+    }
 }
