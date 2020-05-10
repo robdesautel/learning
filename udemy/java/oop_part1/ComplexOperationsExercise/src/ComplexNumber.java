9f4f754f4e0fd1f83649d6fba4bd38a43a1d3929
robdesautel <17968614+robdesautel@users.noreply.github.com>
2020-05-10 10:15:36 -0400

    Setup fields and getters/setters

--- udemy/java/oop_part1/ComplexOperationsExercise/src/ComplexNumber.java
+++ udemy/java/oop_part1/ComplexOperationsExercise/src/ComplexNumber.java
@@ -1,2 +1,29 @@
 public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+
+    public double add(){
+        return 0.0d;
+    }
+
+    public double subtract(){
+        return 0.0d;
+    }
+
+    public double subtract(){
+        return 0.0d;
+    }
 }
