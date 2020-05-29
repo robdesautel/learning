598f97ca6999936825e7adfbe47d38182526c19d
robdesautel <17968614+robdesautel@users.noreply.github.com>
2020-05-28 23:44:01 -0400

    Implemented Customer, Branch, and Bank.

--- udemy/java/TimChallenges/AutoboxingUnboxingChallenge/src/Bank.java
+++ udemy/java/TimChallenges/AutoboxingUnboxingChallenge/src/Bank.java
@@ -1,2 +1,70 @@
+import org.graalvm.compiler.asm.sparc.SPARCAssembler;
+
+import java.util.ArrayList;
+
 public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<>();
+    }
+
+    public boolean addBranch(String branchName){
+        if (findBranch(branchName) == null){
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+        return true;
+    }
+
+    public boolean addCustomer(String branchName, String customerName, double initialAmount) {
+        Branch branch = findBranch(branchName);
+        if(branch != null){
+            return branch.createCustomer(customerName, initialAmount);
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String branchName, String customerName, double transaction){
+        Branch branch = findBranch(branchName);
+        if(branch != null){
+            branch.addTransaction(customerName, transaction);
+            return true;
+        }
+        return false;
+    }
+
+    private Branch findBranch(String branchName){
+        for(Branch branch : branches){
+            if (branch.getName().equals(branchName)){
+                return branch;
+            }
+        }
+        return null;
+    }
+
+    public boolean customerTransaction(String branchName, boolean showTransaction){
+        Branch branch = findBranch(branchName);
+            if(branch != null){
+                System.out.println("Customer details for branch " + branch.getName());
+
+                ArrayList<Customer> branchCustomers = branch.getCustomers();
+                for(Customer customer: branchCustomers){
+                    System.out.println("Customer: " + customer.getName());
+                    if(showTransaction){
+                        System.out.println("Transactions");
+                        ArrayList<Double> customerTransactions = customer.getTransactions();
+                        for(int i = 0; i < customerTransactions.size(); i++){
+                            System.out.println("[ " + (i + 1) + " ] Amount: " + customerTransactions.get(i) );
+                        }
+                    }
+                }
+                return true;
+            } else {
+                return false;
+
+            }
+    }
 }
--- udemy/java/TimChallenges/AutoboxingUnboxingChallenge/src/Branch.java
+++ udemy/java/TimChallenges/AutoboxingUnboxingChallenge/src/Branch.java
@@ -3,115 +3,47 @@ import org.graalvm.compiler.asm.sparc.SPARCAssembler;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class Branch {
+public class Branch{
     private String name;
     private ArrayList<Customer> customers;
 
     public Branch(String name) {
-
         this.name = name;
         this.customers = new ArrayList<>();
-    }
-
-    private int findCustomer(Customer customer){
-        return this.customers.indexOf(customer);
-    }
-
-    private int findCustomer(String customerName){
-        for(int i = 0; i < customers.size(); i++){
-            Customer customer = this. customers.get(i);
-            if(customer.getFullName().equals(customerName)){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    private boolean verifyCustomer(String customerName){
-        return (findCustomer(customerName) >= 0);
-    }
-
-    public void customerDeposits(String customerFirstName, String customerLastName){
-        String customerName = customerFirstName + ", " + customerLastName;
-        if(verifyCustomer(customerName)){
-            for (Customer customer : customers) {
-                System.out.println("Deposits made: " +
-                        customer.deposits());
-            }
-        }
-    }
-
-    public void customerBalances(String customerFirstName, String customerLastName){
-        String customerName = customerFirstName + ", " + customerLastName;
-        if(verifyCustomer(customerName)){
-            for (Customer customer : customers) {
-                System.out.println("Balances were: " +
-                        customer.currentBalances());
-            }
-        }
-    }
-
-    public void customerWithdraws(String customerFirstName, String customerLastName){
-        String customerName = customerFirstName + ", " + customerLastName;
-        if(verifyCustomer(customerName)){
-            for (Customer customer : customers) {
-                System.out.println("Withdraws made: " +
-                        customer.withdraws());
-            }
-        }
-    }
-
-    public void customerBalance(String customerFirstName, String customerLastName) {
-        String customerName = customerFirstName + ", " + customerLastName;
-        if (verifyCustomer(customerName)) {
-            for (Customer customer : this.customers) {
-                System.out.println(customerName + "'s balance is " +
-                        customer.getCurrentBalance());
-            }
-        }
-    }
-
-    public boolean customerDeposit(String customerFirstName,
-                                   String customerLastName,
-                                   double customerDeposit) {
-        String customerName = customerFirstName + ", " + customerLastName;
-        if (verifyCustomer(customerName)) {
-            for(Customer customer: this.customers){
-                customer.depositTransaction(customerDeposit);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean customerWithdraw(String customerFirstName,
-                                   String customerLastName,
-                                   double customerWithdraw) {
-        String customerName = customerFirstName + ", " + customerLastName;
-        if (verifyCustomer(customerName)) {
-            for(Customer customer: this.customers){
-                customer.withdrawTransaction(customerWithdraw);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean addNewCustomer(Customer customer){
-        if(findCustomer(customer.getFullName()) >= 0){
-            System.out.println("Customer already exists!");
-            return false;
-        }
-        customers.add(customer);
-        return true;
     }
 
     public String getName() {
         return name;
     }
 
-
-    public static Branch createBranch(String name){
-        return new Branch(name);
+    public ArrayList<Customer> getCustomers() {
+        return customers;
     }
+
+    public boolean createCustomer(String customerName, double initialAmount){
+        if(findCustomer(customerName) == null){
+            this.customers.add(new Customer(customerName, initialAmount));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addTransaction(String customerName, double amount){
+        Customer existingCustomer = findCustomer(customerName);
+        if(existingCustomer != null){
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+        return false;
+    }
+
+    private Customer findCustomer(String customerName){
+        for(Customer customer: this.customers){
+            if(customer.getName().equals(customerName)){
+                return customer;
+            }
+        }
+        return null;
+    }
+
 }
